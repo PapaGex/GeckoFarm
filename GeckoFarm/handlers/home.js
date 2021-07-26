229743406ce9aab179e8268d5b8936ed8bbc9157
PapaGex <cryptoblockchainjunkie@gmail.com>
2021-07-26 18:33:45 -0400

    ready for class

---
+++ GeckoFarm/handlers/home.js
@@ -0,0 +1,40 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const geckos = require('../data/geckos');
+const species = require('../data/species');
+
+
+module.exports = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathmame === '/' && req.method === 'GET') {
+
+        // implement the logic for showing the html view
+        let filepath = path.normalize(
+            path.join(__dirname, "./views/home/index.html")
+        );
+        fs.readFile(filepath, (err,data) => {
+            if(err){
+                console.log(err);
+                res.writeHead(404,{
+                    'Content-Type': "text/plain"
+                });
+
+                res.write(404);
+                res.end();
+                return;
+            }
+        res.writeHead(200,{
+            'Content-Type': 'text/html'
+        });
+
+        res.write(data);
+        res.end();
+        });
+
+    } else {
+        return true;
+
+    }
+}
--- GeckoFarm/index.js
+++ GeckoFarm/index.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const port = 2077;
-
+const handlers = require('./handlers');
 
 
 
 http.createServer((req, res) => {
+
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
